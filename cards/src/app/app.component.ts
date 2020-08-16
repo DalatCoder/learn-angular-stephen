469647a5e2758606bba4b961f0c1e2699859c23d
TrongHieu <hieuntctk42@gmail.com>
2020-08-16 10:49:57 +0700

    02.Communication between parent and children

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.png',
+      username: 'nature',
+      content: 'I saw this neat tree today',
+    },
+    {
+      title: 'Snowy Moutain',
+      imageUrl: 'assets/moutain.png',
+      username: 'mountainlover',
+      content: 'Here is the picture of a snowy moutain',
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/biking.png',
+      username: 'bikinglover',
+      content: 'I did some biking today',
+    },
+  ];
 }
